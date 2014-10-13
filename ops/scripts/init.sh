b7f21cb408b4ec60b01e590ca877b38f3cd25de5
Hwechul Cho (Derrick) <flrngel@gmail.com>
2014-10-13 11:47:10 +0900

    Update init.sh

--- ops/scripts/init.sh
+++ ops/scripts/init.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-REPO="
-deb http://ftp.daum.net/ubuntu/ trusty main restricted
-deb-src http://ftp.daum.net/ubuntu/ trusty main restricted
-deb http://ftp.daum.net/ubuntu/ trusty universe
-deb-src http://ftp.daum.net/ubuntu/ trusty universe
-deb http://ftp.daum.net/ubuntu/ trusty multiverse
-deb-src http://ftp.daum.net/ubuntu/ trusty multiverse
-"
 #sudo bash -c "echo '$REPO' > /etc/apt/sources.list"
 
 sudo apt-get check
